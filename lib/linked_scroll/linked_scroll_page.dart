9b553da6fe8ed8994119d086e739ae577dfb1579
yakitama5 <rittitarou@yahoo.co.jp>
2024-09-10 15:51:53 +0900

    ✨feat: `linked_scroll`によるレイアウトサンプルの追加

--- lib/linked_scroll/linked_scroll_page.dart
+++ lib/linked_scroll/linked_scroll_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:linked_scroll_controller/linked_scroll_controller.dart';
 
 /// LinkedScroll
 class LinkedScrollPage extends StatefulWidget {
@@ -9,9 +10,104 @@ class LinkedScrollPage extends StatefulWidget {
 }
 
 class _State extends State<LinkedScrollPage> {
+  final LinkedScrollControllerGroup _controllerGroup =
+      LinkedScrollControllerGroup();
+
+  static const imageHeight = 480.0;
+  static const duplicateHeight = 16.0;
+
+  late final ScrollController _appBarController;
+  late final ScrollController _bodyController;
+
+  @override
+  void initState() {
+    super.initState();
+    _appBarController = _controllerGroup.addAndGet();
+    _bodyController = _controllerGroup.addAndGet();
+  }
+
+  @override
+  void dispose() {
+    _appBarController.dispose();
+    _bodyController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      // Body > AppBarの順で配置し、`LinkedScroll`で同期する
+      body: Stack(
+        children: [
+          CustomScrollView(
+            controller: _appBarController,
+            slivers: [
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: imageHeight,
+                title: const Text('Linked Scroll Controller Group'),
+                flexibleSpace: FlexibleSpaceBar(
+                  background: OverflowBox(
+                    child: Image.network(
+                      height: imageHeight,
+                      fit: BoxFit.fitWidth,
+                      'https://placehold.jp/f06b24/ffffff/270x480.png',
+                    ),
+                  ),
+                ),
+              ),
+              const SliverFillRemaining(),
+            ],
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(top: kToolbarHeight),
+              child: CustomScrollView(
+                controller: _bodyController,
+                slivers: [
+                  SliverPadding(
+                    // AppBarとBodyに重なりを持たせて表示
+                    padding: const EdgeInsets.only(
+                      top: imageHeight - kToolbarHeight - duplicateHeight,
+                    ),
+                    sliver: SliverList.separated(
+                      itemBuilder: (context, index) {
+                        if (index <= 0) {
+                          return const _FirstTile();
+                        }
+                        return ListTile(title: Text('Index $index'));
+                      },
+                      separatorBuilder: (_, __) => const Divider(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _FirstTile extends StatelessWidget {
+  const _FirstTile();
+
+  @override
+  Widget build(BuildContext context) {
+    final cs = Theme.of(context).colorScheme;
+
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: cs.primary,
+        borderRadius: const BorderRadius.vertical(
+          top: Radius.circular(16),
+        ),
+      ),
+      child: const SizedBox(
+        height: 160,
+        width: double.infinity,
+      ),
+    );
   }
 }
--- lib/main_linked_scroll.dart
+++ lib/main_linked_scroll.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_laboratory/linked_scroll/linked_scroll_app.dart';
 
+// ignore: missing_provider_scope
 void main() => runApp(const LinkedScrollApp());
