e124027c62520543c4ce4b3df88c0498518b6a03
yakitama5 <rittitarou@yahoo.co.jp>
2024-10-18 11:37:13 +0900

    ✨feat: testable_keyの追加

---
+++ lib/testable_key/src/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TestableKeyApp extends StatelessWidget {
+  const TestableKeyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
