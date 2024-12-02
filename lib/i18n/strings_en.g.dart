884c8e7215d2393e0f31b13fba1ef3a011ea8d0e
yakitama5 <rittitarou@yahoo.co.jp>
2024-12-02 17:24:20 +0900

    ✨feat: slangの追加

---
+++ lib/i18n/strings_en.g.dart
@@ -0,0 +1,65 @@
+///
+/// Generated file. Do not edit.
+///
+// coverage:ignore-file
+// ignore_for_file: type=lint, unused_import
+
+part of 'strings.g.dart';
+
+// Path: <root>
+typedef TranslationsEn = Translations; // ignore: unused_element
+class Translations implements BaseTranslations<AppLocale, Translations> {
+	/// Returns the current translations of the given [context].
+	///
+	/// Usage:
+	/// final i18n = Translations.of(context);
+	static Translations of(BuildContext context) => InheritedLocaleData.of<AppLocale, Translations>(context).translations;
+
+	/// You can call this constructor and build your own translation instance of this locale.
+	/// Constructing via the enum [AppLocale.build] is preferred.
+	Translations({Map<String, Node>? overrides, PluralResolver? cardinalResolver, PluralResolver? ordinalResolver})
+		: assert(overrides == null, 'Set "translation_overrides: true" in order to enable this feature.'),
+		  $meta = TranslationMetadata(
+		    locale: AppLocale.en,
+		    overrides: overrides ?? {},
+		    cardinalResolver: cardinalResolver,
+		    ordinalResolver: ordinalResolver,
+		  ) {
+		$meta.setFlatMapFunction(_flatMapFunction);
+	}
+
+	/// Metadata for the translations of <en>.
+	@override final TranslationMetadata<AppLocale, Translations> $meta;
+
+	/// Access flat map
+	dynamic operator[](String key) => $meta.getTranslation(key);
+
+	late final Translations _root = this; // ignore: unused_field
+
+	// Translations
+	late final TranslationsSampleEn sample = TranslationsSampleEn.internal(_root);
+}
+
+// Path: sample
+class TranslationsSampleEn {
+	TranslationsSampleEn.internal(this._root);
+
+	final Translations _root; // ignore: unused_field
+
+	// Translations
+	String greet({required num amount}) => '${NumberFormat.currency(locale: 'en').format(amount)}';
+	String today({required DateTime date}) => 'Today is ${DateFormat.yMd('en').format(date)}';
+}
+
+/// Flat map(s) containing all translations.
+/// Only for edge cases! For simple maps, use the map function of this library.
+extension on Translations {
+	dynamic _flatMapFunction(String path) {
+		switch (path) {
+			case 'sample.greet': return ({required num amount}) => '${NumberFormat.currency(locale: 'en').format(amount)}';
+			case 'sample.today': return ({required DateTime date}) => 'Today is ${DateFormat.yMd('en').format(date)}';
+			default: return null;
+		}
+	}
+}
+
