8c6e0ff6bb29bf4431b4db1e4d7afc96e489fed8
yakitama5 <rittitarou@yahoo.co.jp>
2025-02-23 20:23:06 +0900

    ✨feat: DynamicSchemeVariantのサンプルを追加

--- apps/dynamic_color_variant/lib/main.dart
+++ apps/dynamic_color_variant/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:dynamic_color_variant/src/app.dart';
+import 'package:flutter/material.dart';
 
 // ignore: missing_provider_scope
 void main() => runApp(const DynamicColorVariantApp());
--- apps/dynamic_color_variant/lib/src/app.dart
+++ apps/dynamic_color_variant/lib/src/app.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 class DynamicColorVariantApp extends StatelessWidget {
   const DynamicColorVariantApp({super.key});
@@ -6,21 +9,83 @@ class DynamicColorVariantApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: _SamplePage(),
     );
   }
 }
 
-class _SamplePage extends StatelessWidget {
+class _SamplePage extends HookWidget {
   const _SamplePage();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('DynamicColorVariant'),
+    final dynamicSchemeVariant = useState(DynamicSchemeVariant.tonalSpot);
+
+    return Theme(
+      data: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue,
+          dynamicSchemeVariant: dynamicSchemeVariant.value,
+        ),
       ),
-      body: const SizedBox.shrink(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('DynamicSchemeVariant'),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                ...DynamicSchemeVariant.values.map(
+                  (v) => RadioListTile<DynamicSchemeVariant>(
+                    value: v,
+                    groupValue: dynamicSchemeVariant.value,
+                    title: Text(v.name),
+                    onChanged: (v) => dynamicSchemeVariant.value = v!,
+                  ),
+                ),
+                const Divider(),
+                Expanded(
+                  child: Column(
+                    children: [
+                      Wrap(
+                        children: [
+                          FilledButton(
+                            onPressed: () {},
+                            child: const Text('Filled'),
+                          ),
+                          FilledButton.tonal(
+                            onPressed: () {},
+                            child: const Text('Filled.tonal'),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('Elevated'),
+                          ),
+                          OutlinedButton(
+                            onPressed: () {},
+                            child: const Text('Outlined'),
+                          ),
+                        ],
+                      ),
+                      Wrap(
+                        children: [
+                          Switch(value: false, onChanged: (v) {}),
+                          Switch(value: true, onChanged: (v) {}),
+                        ],
+                      ),
+                      // const Divider(),
+                      // Text(dynamicSchemeVariant.value.name),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
